85b497301b394b3df5237f3a0ba5c1ac3390d4a8
rossiniDavide <rossini.davide@panettipitagora.edu.it>
2023-09-20 13:24:18 +0200

    previ

--- sito/static/js/script.js
+++ sito/static/js/script.js
@@ -8,21 +8,29 @@ let longitudine = 12.454113036228433;
     console.log(event)
     latitudine = event.coords.latitude
     longitudine = event.coords.longitude
-    CreateMap()
+    CreateMap(latitudine, longitudine)
 
  },
  function (event){
     console.log("L'utente non ha accettato")
     console.log(event)
-    CreateMap()
+    CreateMap(latitudine, longitudine)
     
  }
  )
- function CreateMap(){
-   let map = L.map('map').setView(latitudine,longitudine, 16);
+ function CreateMap(latitudine, longitudine){
+   let map = L.map('map').setView([latitudine,longitudine] ,11);
    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
+}).addTo(map);  
+
+
+let marker = L.marker([latitudine,longitudine]).addTo(map);
+
+map.on("click",function (event) {
+   console.log(event)
+   marker.remove()
+   marker = L.marker([event.latlng.lat, event.latlng,lng]),assto(map)
+})
  }
-let marker = L.marker([51.5, -0.09]).addTo(map);
